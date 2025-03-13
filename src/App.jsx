c58edd7b6c12f50d566da332c5c6fac7f7131566
FaboRobi <fabodudasr@gmail.com>
2025-03-13 11:28:38 +0100

    login page start

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import pollak_logo_light from './assets/pollak_logo_light.png'
+import pollak_logo_dark from './assets/pollak_logo_dark.png'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='h-screen flex justify-center items-center'>
+        <div className='bg-[#F6F6F6] w-[30%] h-[40%] flex justify-center items-center rounded-xl drop-shadow-[0px_4px_4px_rgba(0, 0, 0, 0.10)]'>
+          <div className='w-[50%] flex flex-col justify-center items-center'>
+            <img src={pollak_logo_dark} alt="pollak_logo" />
+            <p>Pollák Kapu</p>
+          </div>
+          <div className='w-[50%] flex justify-center items-center'>
+            asdasdas
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
