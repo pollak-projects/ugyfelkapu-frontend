6d95267d64ee65574bd36e9b40b6c7d53b4fea7c
Róbert Fabó Dudás <fabodudasr@gmail.com>
2025-03-17 08:44:07 +0100

    login go BRRRR

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import pollak_logo_light from './assets/pollak_logo_light.png'
-import pollak_logo_dark from './assets/pollak_logo_dark.png'
-
+import pollak_logo_light from "./assets/pollak_logo_light.png";
+import pollak_logo_dark from "./assets/pollak_logo_dark.png";
 
 function App() {
-
-
   return (
     <>
-      <div className='h-screen flex justify-center items-center'>
-        <div className='bg-[#F6F6F6] w-[30%] h-[40%] flex justify-center items-center rounded-xl drop-shadow-[0px_4px_4px_rgba(0, 0, 0, 0.10)]'>
-          <div className='w-[50%] flex flex-col justify-center items-center'>
+      <div className="h-screen flex justify-center items-center">
+        <div className="bg-[#00FFFF] w-[30%] h-[40%] flex justify-center items-center rounded-xl drop-shadow-[0px_4px_4px_rgba(0, 0, 0, 0.10)]">
+          <div className="w-[50%] flex flex-col justify-center items-center">
             <img src={pollak_logo_dark} alt="pollak_logo" />
-            <p>Pollák Kapu</p>
+            <p className="text-xl font-semibold">Pollák Kapu</p>
           </div>
-          <div className='w-[50%] flex justify-center items-center'>
-            asdasdas
-          </div>
+          <form action="">
+            <div className="w-[50%] grid justify-center items-center gap-6">
+              <h1 className="font-semibold text-3xl">Bejelentkezés</h1>
+              <div className="block">
+                <label htmlFor="omazonosito" className="font-normal ">OM azonosító</label>
+                <input
+                  type="text"
+                  name="omazonosito"
+                  id="om"
+                  className="bg-white rounded-lg border-1 border-opacity-20"
+                />
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
