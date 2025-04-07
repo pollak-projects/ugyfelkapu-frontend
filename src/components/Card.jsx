a4c73b9ccdc198647b16d51a15a3792d09fc7a48
FaboRobi <fabodudasr@gmail.com>
2025-04-07 08:45:15 +0200

    Home pga eelkezdése

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,18 +11,18 @@ const Card = ({
   return (
     <a
       href={href}
-      className={`flex flex-col items-center bg-card border border-main rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-main ${className}`}
+      className={`flex flex-col items-center bg-card rounded-lg shadow-sm md:flex-row hover:bg-main p-3 ${className}`}
     >
       <img
-        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-70 md:rounded-none md:rounded-s-lg"
         src={imageSrc}
         alt={imageAlt}
       />
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-text">
+      <div className="flex flex-col p-4 leading-normal">
+        <h5 className="mb-2 text-2xl font-normal tracking-tight">
           {title}
         </h5>
-        <p className="mb-3 font-normal text-input">{description}</p>
+        <p className="mb-3 font-light text-input">{description}</p>
       </div>
     </a>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,32 @@
 import Layout from "../components/Layout";
+import Card from "../components/Card";
+import img1 from "../assets/cardimg1.png";
+import img2 from "../assets/cardimg2.png";
+import img3 from "../assets/cardimg3.png";
 
 function Home() {
   return (
     <>
       <Layout title={"Főoldal"}>
-
+        <h2 className="pl-3 pb-2 sub-title">Hírek</h2>
+        <div className="card">
+          <Card
+            imageSrc={img1}
+            title={"Top 100!"}
+            description={"A technikumok TOP 100-as listáján a Pollák!"}
+            imageAlt="img1"
+          />
+          <Card
+            imageSrc={img2}
+            title={"Top Képzéseink 2025/2026-os tanévben100!"}
+            description={"Képzéseink 2025/2026-os tanévben"}
+          />
+          <Card
+            imageSrc={img3}
+            title={"Emelik az ösztöndíjat"}
+            description={"A 2025/2026-os tanévtől kezdve minden ösztöndíj 40%-os emelést kap"}
+          />
+        </div>
       </Layout>
     </>
   );
